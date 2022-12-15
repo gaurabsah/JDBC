b125a4047644fa227f9f0157bf313de7129f6cd7
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-15 16:41:03 +0530

    added Student class

---
+++ StudentManagementApp/src/com/students/model/Student.java
@@ -0,0 +1,67 @@
+package com.students.model;
+
+public class Student {
+
+	private int studentId;
+	private String studentName;
+	private int studentPhone;
+	private String studentCity;
+
+	public Student() {
+
+	}
+
+	public Student(int studentId, String studentName, int studentPhone, String studentCity) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentPhone = studentPhone;
+		this.studentCity = studentCity;
+	}
+
+	public Student(String studentName, int studentPhone, String studentCity) {
+		super();
+		this.studentName = studentName;
+		this.studentPhone = studentPhone;
+		this.studentCity = studentCity;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentPhone() {
+		return studentPhone;
+	}
+
+	public void setStudentPhone(int studentPhone) {
+		this.studentPhone = studentPhone;
+	}
+
+	public String getStudentCity() {
+		return studentCity;
+	}
+
+	public void setStudentCity(String studentCity) {
+		this.studentCity = studentCity;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentPhone=" + studentPhone
+				+ ", studentCity=" + studentCity + "]";
+	}
+
+}
